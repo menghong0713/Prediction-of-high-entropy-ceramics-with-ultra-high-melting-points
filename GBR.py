42175782d5ef012ae03dec2dd71f62d0b6f0b005
menghong0713 <91270001+menghong0713@users.noreply.github.com>
2024-12-15 11:33:32 +0800

    Update GBR.py

--- GBR.py
+++ GBR.py
@@ -4,8 +4,7 @@ from sklearn.model_selection import KFold, cross_val_score
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 
-# 假设你有一个特征矩阵 X 和对应的标签向量 y
-data = pd.read_excel('criteria_train.xlsx', header=0, index_col = 0)  # 训练数据文件路径
+data = pd.read_excel('criteria_train.xlsx', header=0, index_col = 0)  
 
 X = data.iloc[:,1:].apply(lambda x: round(x, 4))
 y = data.iloc[:,0].values
@@ -15,10 +14,8 @@ model = make_pipeline(
     GradientBoostingRegressor()
 )
 
-# 训练model模型
 model.fit(X, y)
 
-#交叉验证法
 crossvalidation = KFold(n_splits =129, shuffle=True)
 rmse_score_gbr = cross_val_score(model, X, y, scoring = 'neg_root_mean_squared_error',cv = crossvalidation)
 
